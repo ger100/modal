7a5fb6609a731ce738511a19e6a5db7c79837747
V.Gergely <kojak105@gmail.com>
2020-11-25 23:51:49 +0100

    modal buttons

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+
+function openModal() { 
+    document.getElementsByClassName("modal")[0].style.display = "block";
+    document.getElementsByClassName("overlay")[0].style.display = "block";
+
+}
+
+function closeModal() { 
+    document.getElementsByClassName("modal")[0].style.display = "none";
+    document.getElementsByClassName("overlay")[0].style.display = "none";
+}
