4c51c32fcbe65a3ad8eba86d404a56e0db19e259
V.Gergely <kojak105@gmail.com>
2020-11-26 00:51:57 +0100

    fade in out

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,23 @@
 
 function openModal() { 
-    document.getElementsByClassName("modal")[0].style.display = "block";
-    document.getElementsByClassName("overlay")[0].style.display = "block";
+    //document.getElementsByClassName("modal")[0].style.display = "block";
+    //document.getElementsByClassName("modal")[0].style.visibility = "visible";
+    document.getElementsByClassName("modal")[0].style.opacity = "1";
+    document.getElementsByClassName("modal")[0].style.transition = "opacity 1s";
 
+    //document.getElementsByClassName("overlay")[0].style.display = "block";
+    document.getElementsByClassName("overlay")[0].style.opacity = "1";
+    document.getElementsByClassName("overlay")[0].style.transition = "opacity 1s";
+    document.getElementsByClassName("overlay")[0].style.visibility = "visible";
 }
 
 function closeModal() { 
-    document.getElementsByClassName("modal")[0].style.display = "none";
-    document.getElementsByClassName("overlay")[0].style.display = "none";
+    //document.getElementsByClassName("modal")[0].style.display = "none";
+    document.getElementsByClassName("modal")[0].style.opacity = "0";   
+    document.getElementsByClassName("modal")[0].style.transition = "opacity 1s";
+    
+    //document.getElementsByClassName("overlay")[0].style.display = "none";
+    document.getElementsByClassName("overlay")[0].style.opacity = "0";   
+    document.getElementsByClassName("overlay")[0].style.transition = "opacity 1s";
+    document.getElementsByClassName("overlay")[0].style.visibility = "hidden";
 }
